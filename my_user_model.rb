308d67230a012766b7f2e952abcff87772895322
danny ortiz <ortiz_d@qwasar.io>
2023-04-12 22:47:36 +0000

    added user counter and users arr

--- my_user_model.rb
+++ my_user_model.rb
@@ -1,20 +1,23 @@
 class USER
-
+    @@users = []
+    @@sum = 0
     attr_accesor :firstname, :lastname, :age, :password, :email
 
     def create(user_info)
         user = User.new
+        user.id = @@sum += 1
         user.firstname = user_info[:firstname]
         user.lastname = user_info[:lastname]
         user.age = user_info[:age]
         user.password = user_info[:password]
         user.email = user_info[:email]
         user.save
+        @@users << user
         user
     end
 
     def find(user_id)
-
+        
     end
 
     def all
