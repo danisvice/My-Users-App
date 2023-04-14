a82edab5099a14e697ec468fa1899e7978df142d
danny ortiz <ortiz_d@qwasar.io>
2023-04-14 23:50:17 +0000

    updated

--- my_user_model.rb
+++ my_user_model.rb
@@ -1,26 +1,40 @@
-class USER
-    @@users = []
-    @@sum = 0
-    attr_accesor :firstname, :lastname, :age, :password, :email
+require 'sqlite3'
 
-    def create(user_info)
-        user = User.new
-        user.id = @@sum += 1
-        user.firstname = user_info[:firstname]
-        user.lastname = user_info[:lastname]
-        user.age = user_info[:age]
-        user.password = user_info[:password]
-        user.email = user_info[:email]
-        user.save
-        @@users << user
-        user
+class USER
+    attr_reader :db
+
+    def initialize(db_file)
+        db = SQLite3::Database.new('db.sql')
+        create_table
+    end
+
+    def create_table
+        db.execute 
+        << SQL CREATE TABLE IF NOT EXISTS
+        users (
+            id INTEGER PRIMARY KEY, 
+            firstname TEXT,
+            lastname TEXT,
+            age INTEGER,
+            password TEXT,
+            email TEXT
+        ); 
+          SQL
+    end
+
+    def create(user_info) 
+        firstname, lastname, age, password, email = user_info.values
+        db.execute("INSERT INTO users (firstname, lastname, age, password, email) VALUES (?,?,?,?,?)", 
+                    [firstname,lastname,age,password,email])
+        db.last_insert_row_id
     end
 
     def find(user_id)
-        
+        db.execute("SELECT * FROM users WHERE id=?", [user_id]).first
     end
 
     def all
+        db.execute("SELECT ")
     end
 
     def update(user_id, attribute, value)
