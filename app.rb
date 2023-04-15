9380055c104035e51a9c15c1c2acd13332d07063
danny ortiz <ortiz_d@qwasar.io>
2023-04-15 01:32:55 +0000

    updated

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'json'
+require_relative 'user'
+
+set :port, 8080
+set :bind, '0.0.0.0'
+set :public_folder, 'public'
+set :views, 'views'
+
+enable :sessions 
+
+user_db = User.new('db.sql')
+
+get '/' do 
+    erb :index 
+end
+
+get '/users' do 
+    content_type :json 
+    user_db.all.to_json
+end
+
+post '/users' do 
+    user_info = params.slice('firstname', 'lastname', 'age', 'password', 'email')
+    user_id = user_db.create(user_info)
+    content_type :json
+    user_db.find(user_id).slice('id', 'firstname', 'lastname', 'age', 'email').to_json
+end
+
+post '/sign_in' do
+    email,password = params.values_at('email', 'password')
+    user = user_db.all.find {|u| u [:email] == email && u[:password] == password}
+    if user 
+        session[:user_id] = user[:id]
+        content_type :json
+        user.slice('id', 'firstname', 'lastname', 'age', 'email').to_json
+    else
+        halt 401 , 'Invalid email or password'
+    end
+end
+
+
+
+
+
