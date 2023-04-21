bc0c37096e30982fc4d4265a2ae0c39a48950a9e
danny ortiz <ortiz_d@qwasar.io>
2023-04-21 21:04:21 +0000

    intialie spec complete

--- spec/initialize_spec.rb
+++ spec/initialize_spec.rb
@@ -1,3 +1,11 @@
 require 'sqlite3'
 require '../my_users_model.rb'
 
+describe User do
+describe "#initialize" do
+    it "creates a new instance of the User class with a database connection" do
+      user = User.new
+      expect(user.db).to be_an_instance_of(SQLite3::Database)
+        end
+    end
+end
