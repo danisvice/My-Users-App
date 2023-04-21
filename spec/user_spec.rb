31782b17099fbcf09e000704ce4b0130c218bd3d
danny ortiz <ortiz_d@qwasar.io>
2023-04-21 20:11:21 +0000

    user_spec complete

---
+++ spec/user_spec.rb
@@ -0,0 +1,11 @@
+require 'sqlite3'
+require_relative '../my_user_model'
+
+describe User do
+    before(:each) do
+      @user = User.new
+end
+
+after(:each) do
+    @user.db.execute("DROP TABLE IF EXISTS users;")
+end
