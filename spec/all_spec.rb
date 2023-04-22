75b257591cb45cd010c46e199ad8ab92979f2ab6
danny ortiz <ortiz_d@qwasar.io>
2023-04-22 05:43:57 +0000

    all_spec complete

---
+++ spec/all_spec.rb
@@ -0,0 +1,12 @@
+require 'sqlite3'
+require_relative '../my_user_model.rb'
+
+describe '#all' do 
+    it "returns a hash of all the users in the database" do 
+        user_info_1 = {firstname: "John" lastname: "Doe" age: "25" password: "password123" email: "johndoe@yahoo.com"}
+        user_info_2 = {firstname : "Jane" lastname: "Doe" age: "30" password: "0123" email: "janedoe@gmail.com"}
+        @user.create(user_info_1)
+        @user.create(user_info_2)
+        expect(@user.all).to_eq([{id: 1, firstname: "John", lastname: "Doe", age: 25, email: "johndoe@yahoo.com"}, {id: 2, firstname: "Jane", lastname: "Doe", age: 25, email: "janedoe@gmail.com"}])
+    end
+end
