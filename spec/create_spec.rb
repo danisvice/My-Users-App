695167a7fad2262404ef71713692cd454a8a43da
danny ortiz <ortiz_d@qwasar.io>
2023-04-22 05:21:00 +0000

    create_spec complete

--- spec/create_spec.rb
+++ spec/create_spec.rb
@@ -3,7 +3,7 @@ require_relative '../my_user_model.rb'
 
 describe #create do 
     it "adds a new user to the database" do
-        user_info = {firstname: "John", lastname: "Doe", age: "25", password: "password", email: "john.doe@example.com"}
-        expect (@user.create(user_info)).to_eq(1)
+        user_info = {firstname: "John" lastname: "Doe" age: "25" password: "password123" email: "johndoe@yahoo.com"}
+        expect(@user.create(user_info)).to eq(1)
     end
 end
