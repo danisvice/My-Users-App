a8a2eac2b136d73235c0a1d8d7b85dc0fae6e34a
danny ortiz <ortiz_d@qwasar.io>
2023-04-22 05:44:06 +0000

    find_spec updated

--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -5,6 +5,6 @@ describe "#find" do
     it "returns the user with specified id" do
         user_info = {firstname: "John" lastname: "Doe" age: "25" password: "password123" email: "johndoe@yahoo.com"}
         user_id = @user.create(user_info)
-        expect(@user.find(user_id)).to eq([user_id, "John", "Doe", 30, "password", "johndoe@yahoo.com"])
+        expect(@user.find(user_id)).to eq([user_id, "John", "Doe", 25, "password123", "johndoe@yahoo.com"])
     end
 end
