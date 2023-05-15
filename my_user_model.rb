8132a15ed03acb19a7354cd94471f01f3363c3d5
danny ortiz <ortiz_d@qwasar.io>
2023-05-15 00:07:50 +0000

    created: class ConnectionSQlite class and a user class to faciliate interaction between sql conn. vs user class.

--- my_user_model.rb
+++ my_user_model.rb
@@ -1,51 +1,82 @@
 require 'sqlite3'
 
+class ConnectionSqlite
+  attr_reader :db
+
+  def initialize(db_filename)
+    @db = SQLite3::Database.new(db_filename)
+    create_table
+  end
+
+  def create_table
+    db.execute <<-SQL 
+      CREATE TABLE IF NOT EXISTS
+      users (
+        id INTEGER PRIMARY KEY, 
+        firstname TEXT,
+        lastname TEXT,
+        age INTEGER,
+        password TEXT,
+        email TEXT
+      );
+    SQL
+  end
+
+  def create(user_info)
+    firstname, lastname, age, password, email = user_info.values_at('firstname', 'lastname', 'age', 'password', 'email')
+    db.execute("INSERT INTO users (firstname, lastname, age, password, email) VALUES (?,?,?,?,?)", [firstname,lastname,age,password,email])
+    db.last_insert_row_id
+  end
+
+  def get(user_id)
+    db.execute("SELECT * FROM users WHERE id=?", [user_id]).first
+  end
+
+  def all
+    db.execute("SELECT * FROM users").map do |row|
+      to_h_with_adding_keys(row)
+    end
+  end
+
+  def update(user_id, attribute, value)
+    db.execute("UPDATE users SET #{attribute}=? WHERE id=?", [value, user_id])
+    get(user_id)
+  end
+
+  def destroy(user_id)
+    db.execute("DELETE FROM users WHERE id=?", [user_id])
+  end
+
+  private
+
+  def to_h_with_adding_keys(array)
+    keys = ["id", "firstname", "lastname", "age", "password", "email"]
+    keys.zip(array).to_h.transform_keys(&:to_sym)
+  end
+end
+
 class User
-    attr_reader :db
+  def initialize
+    @connection = ConnectionSqlite.new('db.sql')
+  end
 
-    def initialize
-        @db = SQLite3::Database.new('db.sql')
-        create_table
-    end
+  def create(user_info)
+    @connection.create(user_info)
+  end
 
-    def create_table
-        db.execute <<-SQL 
-        CREATE TABLE IF NOT EXISTS
-        users (
-            id INTEGER PRIMARY KEY, 
-            firstname TEXT,
-            lastname TEXT,
-            age INTEGER,
-            password TEXT,
-            email TEXT
-        );
-          SQL
-    end
+  def find(user_id)
+    @connection.get(user_id)
+  end
 
-    def create(user_info)
-        firstname, lastname, age, password, email = user_info.values_at('firstname', 'lastname', 'age', 'password', 'email')
-        db.execute("INSERT INTO users (firstname, lastname, age, password, email) VALUES (?,?,?,?,?)", [firstname,lastname,age,password,email])
-        db.last_insert_row_id
-      end
+  def all
+    @connection.all
+  end
 
-      def find(user_id)
-        db.execute("SELECT id, firstname, lastname, age, email FROM users WHERE id=?", [user_id]).first
-      end
+  def update(user_id, attribute, value)
+    @connection.update(user_id, attribute, value)
+  end
 
-    def all
-        db.execute("SELECT id, firstname, lastname, age, email FROM users").map do |row|
-          {id: row[0], firstname: row[1], lastname: row[2], age: row[3], email: row[4]}
-        end
-      end
-
-    #instance method
-    def update(user_id, attribute, value)
-        db.execute("UPDATE users SET #{attribute}=? WHERE id=?", [value, user_id])
-        find(user_id)
-      end
-
-    #instance method
-    def destroy(user_id)
-        db.execute("DELETE FROM users WHERE id=?", [user_id])
-    end
+  def destroy(user_id)
+    @connection.destroy(user_id)
+  end
 end
