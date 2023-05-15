bd9ffc91cb12521328ecaf52454c385766d69a09
danny ortiz <ortiz_d@qwasar.io>
2023-05-15 00:11:24 +0000

    updated: User.new('db.sql'); user_db is now an instance of user class; user_db.delete -> user_db.destroy

--- app.rb
+++ app.rb
@@ -1,61 +1,61 @@
 require 'sinatra'
 require 'json'
-require_relative 'my_user_model'
+require_relative 'my_user_model.rb'
 
 set :port, 8080
 set :bind, '0.0.0.0'
 set :public_folder, 'public'
 set :views, 'views'
 
-enable :sessions 
+enable :sessions
 
-user_db = User.new('db.sql')
+user_db = User.new
 
-get '/' do 
-    @users = User.all
-    erb :index 
+get '/' do
+  @users = user_db.all
+  erb :index
 end
 
-get '/users' do 
-    content_type :json 
-    user_db.all.to_json
+get '/users' do
+  content_type :json
+  user_db.all.to_json
 end
 
-post '/users' do 
-    user_info = params.slice('firstname', 'lastname', 'age', 'password', 'email')
-    user_id = user_db.create(user_info)
-    content_type :json
-    user_db.find(user_id).slice('id', 'firstname', 'lastname', 'age', 'email').to_json
+post '/users' do
+  user_info = params.slice('firstname', 'lastname', 'age', 'password', 'email')
+  user_id = user_db.create(user_info)
+  content_type :json
+  user_db.find(user_id).slice('id', 'firstname', 'lastname', 'age', 'email').to_json
 end
 
 post '/sign_in' do
-    email,password = params.values_at('email', 'password')
-    user = user_db.all.find {|u| u [:email] == email && u[:password] == password}
-    if user 
-        session[:user_id] = user[:id]
-        content_type :json
-        user.slice('id', 'firstname', 'lastname', 'age', 'email').to_json
-    else
-        halt 401 , 'Invalid email or password'
-    end
+  email, password = params.values_at('email', 'password')
+  user = user_db.all.find { |u| u[:email] == email && u[:password] == password }
+  if user
+    session[:user_id] = user[:id]
+    content_type :json
+    user.slice('id', 'firstname', 'lastname', 'age', 'email').to_json
+  else
+    halt 401, 'Invalid email or password'
+  end
 end
 
 put '/users' do
-    user_id = session[:user_id]
-    halt 401, "Not signed in" unless user_id
-    new_password = params['new_password']
-    user = user_db.update(user_id, 'password', new_password)
-    content_type :json
-    user.slice('id', 'firstname', 'lastname', 'age', 'email').to_json
+  user_id = session[:user_id]
+  halt 401, 'Not signed in' unless user_id
+  new_password = params['new_password']
+  user = user_db.update(user_id, 'password', new_password)
+  content_type :json
+  user.slice('id', 'firstname', 'lastname', 'age', 'email').to_json
 end
 
 delete '/sign_out' do
-    session.clear
-    status 204
+  session.clear
+  status 204
 end
 
-delete '/users' do 
-    user_id = session[:user_id]
-    halt 401, 'Not signed in' unless user_id
-    user_db.delete(user_id)
+delete '/users' do
+  user_id = session[:user_id]
+  halt 401, 'Not signed in' unless user_id
+  user_db.destroy(user_id)
 end
