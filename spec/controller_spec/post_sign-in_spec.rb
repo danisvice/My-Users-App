bf8fcabd2d1122ee21ea70d334f0a640da000e84
danny ortiz <ortiz_d@qwasar.io>
2023-05-28 05:37:08 +0000

    Updated: curl POST sign-in test

--- spec/controller_spec/post_sign-in_spec.rb
+++ spec/controller_spec/post_sign-in_spec.rb
@@ -3,6 +3,6 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-curl -X POST -H "Content-Type: application/json" \
--d '{"email": "johndoe@example.com", "password": "test1234"}' \
-http://localhost:8080/sign_in
+# POST /sign_in
+curl -X POST -H "Content-Type: application/json" -d '{"email":"john@example.com","password":"password1"}' http://localhost:8080/sign_in
+# Expected response: {"firstname":"John","lastname":"Doe","age":25,"password":"password1","email":"john@example.com"}
