0e02f09089cc9e0037420b67a48fcfd01d6d931b
danny ortiz <ortiz_d@qwasar.io>
2023-05-28 05:38:07 +0000

    Updated: curl PUT_users test

--- spec/controller_spec/put_users_spec.rb
+++ spec/controller_spec/put_users_spec.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-curl -X PUT -H "Content-Type: application/json" \
--d '{"new_password": "newpass1234"}' \
---cookie "rack.session=1234567890" \
-http://localhost:8080/users
+# PUT /users
+curl -X POST -H "Content-Type: application/json" -d '{"email":"john@example.com","password":"password1"}' http://localhost:8080/sign_in
+curl -X PUT -H "Content-Type: application/json" -d '{"new_password":"new_password"}' http://localhost:8080/users
+# Expected response: {"firstname":"John","lastname":"Doe","age":25,"password":"new_password","email":"john@example.com"}
