8b4f242eda26b652125e971bb3f7a2c68b19d6c6
danny ortiz <ortiz_d@qwasar.io>
2023-05-28 22:38:50 +0000

    debugged curl POST/users test

--- spec/controller_spec/post_users_spec.rb
+++ spec/controller_spec/post_users_spec.rb
@@ -4,5 +4,5 @@ require 'uri'
 require 'json'
 
 # POST /users
-curl -X POST -H "Content-Type: application/json" -d '{"firstname":"Jane","lastname":"Doe","age":30,"password":"password2","email":"jane@example.com"}' http://localhost:8080/users
+curl -X POST -H "Content-Type: application/json" -d '{"firstname="Jane&lastname=Doe&age=30&password=password2&email=jane@example.com"}' http://localhost:8080/users
 # Expected response: {"firstname":"Jane","lastname":"Doe","age":30,"password":"password2","email":"jane@example.com"}
